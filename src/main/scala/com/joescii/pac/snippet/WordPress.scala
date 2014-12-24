b6aaaab85c5a025ac3d2b8ada619784b53ac1bc7
Joe Barnes <barnesjd@gmail.com>
2014-12-24 13:19:05 -0600

    Cleaned up post with Lift stuff

--- src/main/scala/com/joescii/pac/snippet/WordPress.scala
+++ src/main/scala/com/joescii/pac/snippet/WordPress.scala
@@ -1,13 +1,38 @@
 package com.joescii.pac
 package snippet
 
+import net.liftweb.util.ClearNodes
 import net.liftweb.util.Helpers._
 
 import scala.xml.NodeSeq
 
 object WordPress {
-  def render(post:NodeSeq):NodeSeq = {
-    val extractor = ".post ^^" #> "noop"
-    <lift:surround with="default" at="content">{extractor(post)}</lift:surround>
+  def render(page:NodeSeq):NodeSeq = {
+    val extractPost = ".post ^^" #> "noop"
+    val extractComments = ".commentlist ^^" #> "noop"
+    val cleanPostMeta = ".post-meta *" #> {
+      ".byline" #> ClearNodes &
+      ".author" #> ClearNodes &
+      ".comments-link" #> ClearNodes
+    }
+    val stripAds  = "#wordads-preview-parent" #> ClearNodes
+    val stripShareLikeRelated = "#jp-post-flair" #> ClearNodes
+    val stripPostData = ".post-data" #> ClearNodes
+    val stripPostEdit = ".post-edit" #> ClearNodes
+    val stripNavigation = ".navigation" #> ClearNodes
+    val surround:NodeSeq => NodeSeq = { ns => <lift:surround with="default" at="content">{ns}</lift:surround> }
+
+    val post = (extractPost &
+      cleanPostMeta &
+      stripAds &
+      stripShareLikeRelated &
+      stripPostData &
+      stripPostEdit &
+      stripNavigation).apply(page)
+
+    val commentHeader = <hr></hr> ++ <h5 class="olde">Olde Comments</h5>
+    val comments = extractComments.apply(page)
+
+    surround(post ++ commentHeader ++ comments)
   }
 }
