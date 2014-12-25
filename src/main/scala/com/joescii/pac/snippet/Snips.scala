81c45cf76a63d2e998c3acac5f562622b83f53c1
Joe Barnes <barnesjd@gmail.com>
2014-12-25 15:37:57 -0600

    More clean up of the vintage posts and the foundation template

--- src/main/scala/com/joescii/pac/snippet/Snips.scala
+++ src/main/scala/com/joescii/pac/snippet/Snips.scala
@@ -9,12 +9,13 @@ object WordPress {
 
   def render(page:NodeSeq):NodeSeq = {
     val extractPost = ".post ^^" #> "noop"
-    val extractComments = ".commentlist ^^" #> "noop"
     val cleanPostMeta = ".post-meta *" #> {
       ".byline" #> ClearNodes &
         ".author" #> ClearNodes &
         ".comments-link" #> ClearNodes
     }
+    val extractTitle = ".post-title ^*" #> "noop"
+    val insertTitle = ".post-title" #> <h2 class="post-title">{extractTitle(page)}</h2>
     val stripAds  = "#wordads-preview-parent" #> ClearNodes
     val stripShareLikeRelated = "#jp-post-flair" #> ClearNodes
     val stripPostData = ".post-data" #> ClearNodes
@@ -24,14 +25,21 @@ object WordPress {
 
     val post = (extractPost &
       cleanPostMeta &
+      insertTitle &
       stripAds &
       stripShareLikeRelated &
       stripPostData &
       stripPostEdit &
       stripNavigation).apply(page)
 
+    val extractComments = ".commentlist ^^" #> "noop"
+    val stripReplies = ".reply" #> ClearNodes
+    val stripEdits = ".comment-edit-link" #> ClearNodes
+
     val commentHeader = <hr></hr> ++ <h5 class="olde">Olde Comments</h5>
-    val comments = extractComments.apply(page)
+    val comments = (extractComments &
+      stripReplies &
+      stripEdits).apply(page)
 
     surround(post ++ commentHeader ++ comments)
   }
