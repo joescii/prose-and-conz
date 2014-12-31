c566b0f96a09a2b1bbcbecf7e1877bca710ff0ff
Joe Barnes <barnesjd@gmail.com>
2014-12-31 15:54:44 -0600

    Removing debug ls's

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -11,6 +11,4 @@ wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 unzip packer_0.7.5_linux_amd64.zip
 cd ..
 
-ls -l
-
 ./packer/packer build ./web-srv-packer.json
--- deploy/jetty.sh
+++ deploy/jetty.sh
@@ -4,4 +4,3 @@ wget http://download.eclipse.org/jetty/8.1.16.v20140903/dist/jetty-distribution-
 sudo mkdir /usr/lib/jetty
 unzip jetty-distribution-8.1.16.v20140903.zip
 sudo mv ./jetty-distribution-8.1.16.v20140903/* /usr/lib/jetty
-ls -l /usr/lib/jetty
