267690c999a2c4752c117a1fe06149a8f30447de
Joe Barnes <barnesjd@gmail.com>
2014-12-31 14:48:02 -0600

    Correcting the path to my packer json file

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -6,4 +6,4 @@ wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 unzip packer_0.7.5_linux_amd64.zip
 cd ..
 
-./packer/packer build ./deploy/web-srv-packer.json
+./packer/packer build ./web-srv-packer.json
