abd69ade9a542994323023d4130164c81ff54e52
Joe Barnes <barnesjd@gmail.com>
2014-12-31 16:26:14 -0600

    Completely redoing the jetty install per their documentation to set it up as a service

--- deploy/jetty.sh
+++ deploy/jetty.sh
@@ -1,8 +1,27 @@
 #!/bin/bash -eux
 
-cd /usr/lib
+mkdir -p /opt/jetty
+mkdir -p /opt/web/mybase
+mkdir -p /opt/jetty/temp
+
+sudo useradd --user-group --shell /bin/false --home-dir /opt/jetty/temp jetty
+
+chown --recursive jetty /opt/jetty
+chown --recursive jetty /opt/web/mybase
+chown --recursive jetty /opt/jetty/temp
+
+cd /opt/jetty
 sudo wget http://download.eclipse.org/jetty/8.1.16.v20140903/dist/jetty-distribution-8.1.16.v20140903.zip
 sudo unzip jetty-distribution-8.1.16.v20140903.zip
 sudo rm jetty-distribution-8.1.16.v20140903.zip
-sudo mv jetty-distribution-8.1.16.v20140903 jetty
-sudo mv /tmp/proseandconz.war /usr/lib/jetty/webapps
+
+java -jar /opt/jetty/jetty-distribution-9.1.0.v20131115/start.jar --add-to-start=deploy,http,logging
+
+mv /tmp/proseandconz.war /opt/web/mybase/webapps
+
+# Change port to 80?
+
+cp /opt/jetty/jetty-distribution-9.1.0.v20131115/bin/jetty.sh /etc/init.d/jetty
+echo "JETTY_HOME=/opt/jetty/jetty-distribution-9.1.0.v20131115" > /etc/default/jetty
+echo "JETTY_BASE=/opt/web/mybase" >> /etc/default/jetty
+echo "TMPDIR=/opt/jetty/temp" >> /etc/default/jetty
