491135dc0ec10fc4d1c1c8802f48eaf428810b6f
Joe Barnes <barnesjd@gmail.com>
2014-12-31 15:55:46 -0600

    Trying yet another approach for jetty

--- deploy/jetty.sh
+++ deploy/jetty.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eux
 
-wget http://download.eclipse.org/jetty/8.1.16.v20140903/dist/jetty-distribution-8.1.16.v20140903.zip
-sudo mkdir /usr/lib/jetty
-unzip jetty-distribution-8.1.16.v20140903.zip
-sudo mv ./jetty-distribution-8.1.16.v20140903/* /usr/lib/jetty
+cd /usr/lib
+sudo wget http://download.eclipse.org/jetty/8.1.16.v20140903/dist/jetty-distribution-8.1.16.v20140903.zip
+sudo unzip jetty-distribution-8.1.16.v20140903.zip
+sudo rm jetty-distribution-8.1.16.v20140903.zip
+sudo mv jetty-distribution-8.1.16.v20140903 jetty
