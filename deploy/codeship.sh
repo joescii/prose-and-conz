b10d14981524738fadcd763a645fa20263e1473b
Joe Barnes <barnesjd@gmail.com>
2015-01-03 09:34:53 -0600

    Writing terraform state file to s3 bucket

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -5,22 +5,31 @@ mv ./target/scala-2.11/prose-and-conz*.war ./deploy/proseandconz.war
 
 cd ./deploy
 
+# Install packer
 # mkdir packer
 # cd packer
 # wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 # unzip packer_0.7.5_linux_amd64.zip
 # cd ..
 
+# Build the AMI for our server
 # ./packer/packer build ./web-srv-packer.json
 
+# Install terraform
 mkdir terraform
 cd terraform
 wget https://dl.bintray.com/mitchellh/terraform/terraform_0.3.5_linux_amd64.zip
 unzip terraform_0.3.5_linux_amd64.zip
 cd ..
 
+# Get the terraform state
+pip install awscli
+
+# Update the AWS infrastructure
 ./terraform/terraform plan -var "access_key=${AWS_ACCESS_KEY_ID}" -var "secret_key=${AWS_SECRET_ACCESS_KEY}"
 ./terraform/terraform apply -var "access_key=${AWS_ACCESS_KEY_ID}" -var "secret_key=${AWS_SECRET_ACCESS_KEY}"
 
-ls -l
+# Save the terraform state for next time
 cat ./terraform.tfstate
+aws s3 cp ./terraform.tfstate s3://proseandconz/terraform/terraform.tfstate
+
