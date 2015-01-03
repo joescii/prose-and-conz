124090407382c5e3a598b5daadc9ce26dc12a159
Joe Barnes <barnesjd@gmail.com>
2015-01-03 13:31:32 -0600

    Changing the healthy threshold to 1 to see if the ELB comes up faster

--- deploy/aws-infra.tf
+++ deploy/aws-infra.tf
@@ -81,7 +81,7 @@ resource "aws_elb" "pac-elb" {
   }
  
   health_check {
-    healthy_threshold = 2
+    healthy_threshold = 1
     unhealthy_threshold = 2
     timeout = 3
     target = "HTTP:8080/proseandconz/"
