30f414e66bd7dfcbaa7ffa83d7e9e3039fa15cd3
Joe Barnes <barnesjd@gmail.com>
2015-01-02 20:43:49 -0600

    Hello, Terraformgit add -A

---
+++ deploy/aws-infra.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+    region = "${var.region}"
+}
+
+resource "aws_security_group" "default" {
+    name = "terraform_example"
+    description = "Used in the terraform"
+
+    # SSH access from anywhere
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # HTTP access from anywhere
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -11,4 +11,13 @@ wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
 unzip packer_0.7.5_linux_amd64.zip
 cd ..
 
-./packer/packer build ./web-srv-packer.json
+# ./packer/packer build ./web-srv-packer.json
+
+mkdir terraform
+cd terraform
+wget https://dl.bintray.com/mitchellh/terraform/terraform_0.3.5_linux_amd64.zip
+unzip terraform_0.3.5_linux_amd64.zip
+cd ..
+
+./terraform/terraform plan -var "access_key=${AWS_ACCESS_KEY_ID}" -var "secret_key=${AWS_SECRET_ACCESS_KEY}"
+./terraform/terraform apply -var "access_key=${AWS_ACCESS_KEY_ID}" -var "secret_key=${AWS_SECRET_ACCESS_KEY}"
