edc967d24cc4498d34c9a9331aa0776864456f71
Joe Barnes <barnesjd@gmail.com>
2015-01-03 14:21:15 -0600

    First attempt to run the full deployment with both packer and terraform hooked up

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -13,11 +13,11 @@ mv ./target/scala-2.11/prose-and-conz*.war ./deploy/proseandconz.war
 cd ./deploy
 
 # Install packer
-# mkdir packer
-# cd packer
-# wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
-# unzip packer_0.7.5_linux_amd64.zip
-# cd ..
+mkdir packer
+cd packer
+wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+unzip packer_0.7.5_linux_amd64.zip
+cd ..
 
 # Install terraform
 mkdir terraform
@@ -26,12 +26,20 @@ wget https://dl.bintray.com/mitchellh/terraform/terraform_0.3.5_linux_amd64.zip
 unzip terraform_0.3.5_linux_amd64.zip
 cd ..
 
-# Install aws cli for s3
+# Install aws cli
 pip install awscli
+
+# Install jq 
+wget http://stedolan.github.io/jq/download/linux64/jq
+chmod 700 ./jq
 
 
 # Build the AMI for our server
-# ./packer/packer build ./web-srv-packer.json
+./packer/packer build -var timestamp=${timestamp} ./web-srv-packer.json
+
+# Query for the AMI ID just created by packer, based on the timestamp tag
+aws ec2 describe-images --filters Name=tag:Timestamp,Values=${timestamp} > ami.json
+PAC_AMI_ID=`./jq '.Images[0].ImageId' ami.json`
 
 # Get the current terraform state
 aws s3 cp s3://proseandconz/terraform/terraform.tfstate ./terraform.tfstate 
@@ -40,11 +48,11 @@ aws s3 cp s3://proseandconz/terraform/terraform.tfstate ./terraform.tfstate
 ./terraform/terraform plan  \
   -var "access_key=${AWS_ACCESS_KEY_ID}" \
   -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
-  -var "pac_ami_id=ami-48bcd620" 
+  -var "pac_ami_id=${PAC_AMI_ID}" 
 ./terraform/terraform apply \
   -var "access_key=${AWS_ACCESS_KEY_ID}" \
   -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
-  -var "pac_ami_id=ami-48bcd620" 
+  -var "pac_ami_id=${PAC_AMI_ID}" 
 
 # Save the terraform state 
 cat ./terraform.tfstate
