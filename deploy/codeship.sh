896f68a8f9c4dcbe5575a21c26e8c6cfe09a6113
Joe Barnes <barnesjd@gmail.com>
2015-01-02 20:54:19 -0600

    Temprorarily commenting packer stuff to play with terraform. Added an ls -l and a cat to see about this state file terraform writes

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -5,11 +5,11 @@ mv ./target/scala-2.11/prose-and-conz*.war ./deploy/proseandconz.war
 
 cd ./deploy
 
-mkdir packer
-cd packer
-wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
-unzip packer_0.7.5_linux_amd64.zip
-cd ..
+# mkdir packer
+# cd packer
+# wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
+# unzip packer_0.7.5_linux_amd64.zip
+# cd ..
 
 # ./packer/packer build ./web-srv-packer.json
 
@@ -21,3 +21,6 @@ cd ..
 
 ./terraform/terraform plan -var "access_key=${AWS_ACCESS_KEY_ID}" -var "secret_key=${AWS_SECRET_ACCESS_KEY}"
 ./terraform/terraform apply -var "access_key=${AWS_ACCESS_KEY_ID}" -var "secret_key=${AWS_SECRET_ACCESS_KEY}"
+
+ls -l
+cat ./terraform.tfstate
