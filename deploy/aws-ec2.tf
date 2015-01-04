4820818fc8154d9559ca4a671d3453b518ad6c17
Joe Barnes <barnesjd@gmail.com>
2015-01-04 12:48:03 -0600

    Defining a VPC in terraform with private/public subnets and other such good things

--- deploy/aws-ec2.tf
+++ deploy/aws-ec2.tf
@@ -1,12 +1,7 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region = "${var.region}"
-}
-
 resource "aws_security_group" "pac_instance_sg" {
   name = "pac-instance-sg"
   description = "SG applied to each proseandconz app server instance"
+	vpc_id = "${aws_vpc.default.id}"
 
   # HTTP access from anywhere
   ingress {
@@ -20,6 +15,7 @@ resource "aws_security_group" "pac_instance_sg" {
 resource "aws_security_group" "pac_elb_sg" {
   name = "pac-elb-sg"
   description = "SG applied to the proseandconz elastic load balancer"
+	vpc_id = "${aws_vpc.default.id}"
 
   # HTTP access from anywhere
   ingress {
@@ -33,7 +29,7 @@ resource "aws_security_group" "pac_elb_sg" {
 resource "aws_instance" "pac1" {
   ami = "${var.pac_ami_id}"
   instance_type = "t1.micro"
-  subnet_id = "${var.subnet_d}"
+  subnet_id = "${aws_subnet.us-east-1b-private.id}"
   security_groups = ["${aws_security_group.pac_instance_sg.id}"]
   key_name = "joe-pac"
   tags {
@@ -48,7 +44,7 @@ resource "aws_instance" "pac1" {
 resource "aws_instance" "pac2" {
   ami = "${var.pac_ami_id}"
   instance_type = "t1.micro"
-  subnet_id = "${var.subnet_c}"
+  subnet_id = "${aws_subnet.us-east-1d-private.id}"
   security_groups = ["${aws_security_group.pac_instance_sg.id}"]
   key_name = "joe-pac"
   tags {
@@ -62,7 +58,8 @@ resource "aws_instance" "pac2" {
 
 resource "aws_elb" "pac-elb" {
   name = "${var.pac_elb_name}"
-  subnets = ["${var.subnet_d}", "${var.subnet_c}"]
+	vpc_id = "${aws_vpc.default.id}"
+  subnets = ["${aws_subnet.us-east-1b-private.id}", "${aws_subnet.us-east-1d-private.id}"]
   security_groups = ["${aws_security_group.pac_elb_sg.id}"]
   
   # Must manually set the session stickiness for now.  See https://github.com/hashicorp/terraform/issues/656
--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,19 +1,16 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "aws_key_name" {
+  default = "joe-pac"
+}
+variable "aws_nat_ami" {
+	default = "ami-2e1bc047"
+}
+variable "aws_ubuntu_ami" {
+	default = "ami-b227efda"
+}
 variable "region" {
   default = "us-east-1"
-}
-variable "subnet_b" {
-  default = "subnet-80e32cab"
-}
-variable "subnet_c" {
-  default = "subnet-bce063cb"
-}
-variable "subnet_d" {
-  default = "subnet-c00ba899"
-}
-variable "subnet_e" {
-  default = "subnet-48432a72"
 }
 
 variable "pac_ami_id" {}
