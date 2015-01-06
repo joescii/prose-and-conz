0521d3efbb9ff2219ac0a8f85fb0983c48980f9a
Joe Barnes <barnesjd@gmail.com>
2015-01-06 14:25:24 -0600

    Trying a CNAME record for the ELB name

--- deploy/aws-route53.tf
+++ deploy/aws-route53.tf
@@ -3,13 +3,13 @@ resource "aws_route53_zone" "primary" {
 }
 
 # Alias records are not currently supported :( https://github.com/hashicorp/terraform/issues/28
-#resource "aws_route53_record" "www" {
-#   zone_id = "${aws_route53_zone.primary.zone_id}"
-#   name = "joescii.com."
-#   type = "A"
-#   ttl = "300"
-#   records = ["${aws_elb.pac-elb.dns_name}"]
-#}
+resource "aws_route53_record" "www" {
+   zone_id = "${aws_route53_zone.primary.zone_id}"
+   name = "joescii.com."
+   type = "CNAME"
+   ttl = "60"
+   records = ["${aws_elb.pac-elb.dns_name}"]
+}
 
 resource "aws_route53_record" "ns" {
    zone_id = "${aws_route53_zone.primary.zone_id}"
