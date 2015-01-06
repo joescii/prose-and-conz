ec55290cf3a951957c83edf69d4e690a8289c3e1
Joe Barnes <barnesjd@gmail.com>
2015-01-06 11:11:52 -0600

    Putting packer back

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -37,12 +37,11 @@ chmod 700 ./waitFor.sh
 
 
 # Build the AMI for our server
-# ./packer/packer build -var timestamp=${timestamp} ./web-srv-packer.json
+./packer/packer build -var timestamp=${timestamp} ./web-srv-packer.json
 
 # Query for the AMI ID just created by packer, based on the timestamp tag
 aws ec2 describe-images --filters Name=tag:Timestamp,Values=${timestamp} > ami.json
-# PAC_AMI_ID=`./jq --raw-output '.Images[0].ImageId' ami.json`
-PAC_AMI_ID=ami-6ca3d404
+PAC_AMI_ID=`./jq --raw-output '.Images[0].ImageId' ami.json`
 
 # Get the current terraform state
 aws s3 cp s3://proseandconz/terraform/terraform.tfstate ./terraform.tfstate 
