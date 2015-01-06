beb6e90366c669a5ec764974ea01f12720c417ba
Joe Barnes <barnesjd@gmail.com>
2015-01-05 22:19:40 -0600

    Bumping up the max tries to allow for 10 minutes of waiting on the ELB to be ready

--- deploy/aws-ec2.tf
+++ deploy/aws-ec2.tf
@@ -89,7 +89,7 @@ resource "aws_elb" "pac-elb" {
   }
   
   provisioner "local-exec" {
-    command = "./waitFor.sh http://${aws_elb.pac-elb.dns_name}"
+    command = "./waitFor.sh http://${aws_elb.pac-elb.dns_name} 5 120"
   }
 }
 
