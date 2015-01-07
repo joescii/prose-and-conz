6e30e3e8bcfae2cab1cf67376590b836e6cae981
Joe Barnes <barnesjd@gmail.com>
2015-01-07 10:01:29 -0600

    Fixing some sinful 3-space tabbing

--- deploy/aws-route53.tf
+++ deploy/aws-route53.tf
@@ -1,41 +1,41 @@
 resource "aws_route53_zone" "primary" {
-   name = "joescii.com"
+  name = "joescii.com"
 }
 
 # Alias records are not currently supported :( https://github.com/hashicorp/terraform/issues/28
 #resource "aws_route53_record" "www" {
-#   zone_id = "${aws_route53_zone.primary.zone_id}"
-#   name = "joescii.com."
-#   type = "A"
-#   ttl = "300"
-#   records = ["${aws_elb.pac-elb.dns_name}"]
+#  zone_id = "${aws_route53_zone.primary.zone_id}"
+#  name = "joescii.com."
+#  type = "A"
+#  ttl = "300"
+#  records = ["${aws_elb.pac-elb.dns_name}"]
 #}
 
 resource "aws_route53_record" "ns" {
-   zone_id = "${aws_route53_zone.primary.zone_id}"
-   name = "joescii.com."
-   type = "NS"
-   ttl = "172800"
-   records = ["ns-763.awsdns-31.net.", 
-     "ns-1530.awsdns-63.org.",
-     "ns-1566.awsdns-03.co.uk.",
-     "ns-340.awsdns-42.com."
-   ]
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "joescii.com."
+  type = "NS"
+  ttl = "172800"
+  records = ["ns-763.awsdns-31.net.", 
+    "ns-1530.awsdns-63.org.",
+    "ns-1566.awsdns-03.co.uk.",
+    "ns-340.awsdns-42.com."
+  ]
 }
 
 resource "aws_route53_record" "soa" {
-   zone_id = "${aws_route53_zone.primary.zone_id}"
-   name = "joescii.com."
-   type = "SOA"
-   ttl = "900"
-   records = ["ns-1530.awsdns-63.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "joescii.com."
+  type = "SOA"
+  ttl = "900"
+  records = ["ns-1530.awsdns-63.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
 }
 
 resource "aws_route53_record" "cname" {
-   zone_id = "${aws_route53_zone.primary.zone_id}"
-   name = "www.joescii.com."
-   type = "CNAME"
-   ttl = "300"
-   records = ["joescii.com"]
+  zone_id = "${aws_route53_zone.primary.zone_id}"
+  name = "www.joescii.com."
+  type = "CNAME"
+  ttl = "300"
+  records = ["joescii.com"]
 }
 
