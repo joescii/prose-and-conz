6afb88f9e0912d8dd7255ca293928357070ad775
Joe Barnes <barnesjd@gmail.com>
2015-01-24 19:47:02 -0600

    Cleaning up LazyLoadedPosts

--- src/main/scala/com/joescii/pac/lib/LazyLoadedPosts.scala
+++ src/main/scala/com/joescii/pac/lib/LazyLoadedPosts.scala
@@ -2,21 +2,14 @@ package com.joescii.pac
 package lib
 
 import net.liftweb.http.JavaScriptResponse
-import net.liftweb.http.js.JE.JsRaw
 import net.liftweb.http.js.JsCmds._
 import net.liftweb.http.rest.RestHelper
 
 object LazyLoadedPosts extends RestHelper {
-  import model.posts
-  def responseForPost(id:String) = {
-    val post =
-      posts.find(_.uid == id)
-      .map { p =>
-        SetHtml(id, p.html)
-      }
-
-    post getOrElse Noop
-  }
+  def responseForPost(id:String) =
+    model.posts.find(_.uid == id)
+      .map(p => SetHtml(id, p.html))
+      .getOrElse(Noop)
 
   serve {
     case "lazy-post" :: id :: Nil Get _ => new JavaScriptResponse(responseForPost(id), List(), List(), 200)
