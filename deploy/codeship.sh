3c42fecafc3b349f28f6ef327febe34058654e59
Joe Barnes <barnesjd@gmail.com>
2015-01-31 13:35:15 -0600

    Restoring terraform infrastructure

--- deploy/codeship.sh
+++ deploy/codeship.sh
@@ -35,35 +35,38 @@ chmod 700 ./jq
 
 chmod 700 ./*.sh
 
-## Archive the war file
-#aws s3 cp ./root.war s3://proseandconz/deployments/pac-${timestamp}.war
-#
-#if [ -z "$PAC_AMI_ID" ]; then
-#  # Build the AMI for our server
-#  ./packer/packer build -var timestamp=${timestamp} ./web-srv-packer.json
-#
-#  # Query for the AMI ID just created by packer, based on the timestamp tag
-#  aws ec2 describe-images --filters Name=tag:Timestamp,Values=${timestamp} > ami.json
-#  PAC_AMI_ID=`./jq --raw-output '.Images[0].ImageId' ami.json`
-#fi
+# Archive the war file
+aws s3 cp ./root.war s3://proseandconz/deployments/pac-${timestamp}.war
+
+if [ -z "$PAC_AMI_ID" ]; then
+  # Build the AMI for our server
+  ./packer/packer build -var timestamp=${timestamp} ./web-srv-packer.json
+
+  # Query for the AMI ID just created by packer, based on the timestamp tag
+  aws ec2 describe-images --filters Name=tag:Timestamp,Values=${timestamp} > ami.json
+  PAC_AMI_ID=`./jq --raw-output '.Images[0].ImageId' ami.json`
+fi
 
 # Get the current terraform state
-aws s3 cp s3://proseandconz/terraform/terraform.tfstate ./terraform.tfstate 
+#aws s3 cp s3://proseandconz/terraform/terraform.tfstate ./terraform.tfstate
 
 PAC_LAUNCH_CONFIG_NAME=pac-launch-config-${timestamp}
 # Update the AWS infrastructure
-./terraform/terraform destroy -force  \
+./terraform/terraform plan  \
   -var "access_key=${AWS_ACCESS_KEY_ID}" \
   -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
   -var "pac_ami_id=${PAC_AMI_ID}" \
   -var "timestamp=${timestamp}" 
-#./terraform/terraform apply \
-#  -var "access_key=${AWS_ACCESS_KEY_ID}" \
-#  -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
-#  -var "pac_ami_id=${PAC_AMI_ID}" \
-#  -var "timestamp=${timestamp}"
+./terraform/terraform apply \
+  -var "access_key=${AWS_ACCESS_KEY_ID}" \
+  -var "secret_key=${AWS_SECRET_ACCESS_KEY}" \
+  -var "pac_ami_id=${PAC_AMI_ID}" \
+  -var "timestamp=${timestamp}"
+
+# If ever needed...
+#./terraform/terraform destroy -force  \
 
 # Save the terraform state 
-#cat ./terraform.tfstate
-#aws s3 cp ./terraform.tfstate s3://proseandconz/terraform/terraform.tfstate
+cat ./terraform.tfstate
+aws s3 cp ./terraform.tfstate s3://proseandconz/terraform/terraform.tfstate
 
