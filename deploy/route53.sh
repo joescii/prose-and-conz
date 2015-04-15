07818223db3489179e2536d543795dccce2458bf
Anthony Brodard <anthony.brodard@fullsix.com>
2015-04-15 16:20:04 +0200

    Move tmp files into dedicated dir + customize name

--- deploy/route53.sh
+++ deploy/route53.sh
@@ -11,21 +11,26 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
+if [ ! -e ./r53 ]; then
+  mkdir ./r53
+fi
+
+
 zone=$1
 elbName=$2
 
 # File to contain our provisioning settings for Route 53
-rel_json=./route53.json
+rel_json=./r53/route53.json_$zone
 abs_json=$(cd $(dirname "${rel_json}") && pwd -P)/$(basename "${rel_json}")
 
 # Describe the ELB to get the zone ID and DNS
-aws elb describe-load-balancers --load-balancer-name ${elbName} > elb.json
-elbZone=`./jq --raw-output '.LoadBalancerDescriptions[0].CanonicalHostedZoneNameID' elb.json`
-elbDns=`./jq --raw-output '.LoadBalancerDescriptions[0].DNSName' elb.json`
+aws elb describe-load-balancers --load-balancer-name ${elbName} > ./r53/elb.json_$zone
+elbZone=`./jq --raw-output '.LoadBalancerDescriptions[0].CanonicalHostedZoneNameID' ./r53/elb.json_$zone`
+elbDns=`./jq --raw-output '.LoadBalancerDescriptions[0].DNSName' ./r53/elb.json_$zone`
 
 # Describe the Zone to get its Name
-aws route53 get-hosted-zone --id ${zone} > r53.json
-r53Name=`./jq --raw-output '.HostedZone.Name' r53.json`
+aws route53 get-hosted-zone --id ${zone} > ./r53/r53.json_$zone
+r53Name=`./jq --raw-output '.HostedZone.Name' ./r53/r53.json_$zone`
 
 # Write our command for AWS Route 53 to a json file
 cat > ${rel_json} <<EOF
@@ -52,17 +57,17 @@ EOF
 aws route53 change-resource-record-sets \
   --hosted-zone-id ${zone} \
   --change-batch file://${abs_json} \
-  > ./route53-change.json
+  > ./r53/route53-change.json_$zone
 
 # Get the ID for our change request
-changeId=`./jq --raw-output '.ChangeInfo.Id' ./route53-change.json`
+changeId=`./jq --raw-output '.ChangeInfo.Id' ./r53/route53-change.json_$zone`
 status=PENDING
 
 # Poll the status of our change request until it is "INSYNC"
 while [ "${status}" = "PENDING" ]; do
   sleep 5
-  aws route53 get-change --id ${changeId} > ./route53-change.json
-  status=`./jq --raw-output '.ChangeInfo.Status' ./route53-change.json`
+  aws route53 get-change --id ${changeId} > ./r53/route53-change.json_$zone
+  status=`./jq --raw-output '.ChangeInfo.Status' ./r53/route53-change.json_$zone`
   echo "Route53 ALIAS status: ${status}"
 done
 
